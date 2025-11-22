175eb8ac055ba1856c31d534acfda423c050c1ae
Gabriele Laguna <122527628+glag2@users.noreply.github.com>
2025-11-22 19:53:01 +0100

    Add ADB connection command to autostart script

--- code/Autostart/autostart_manager.sh
+++ code/Autostart/autostart_manager.sh
@@ -1,6 +1,7 @@
 sleep 15
 nohup bash -c 'waydroid app launch app.organicmaps.web' >/dev/null 2>&1 < /dev/null &
 sleep 40
+waydroid adb connect
 gpsctl -n /dev/ttyACM0  &
 gpsctl -n /dev/gps0  &
 exec /usr/bin/lxterminal -e "bash -c '/home/gabri/Desktop/Waydroid/geobridge-gpsd.sh; exec bash'" &
